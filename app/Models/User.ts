a6e3198875a35cdd98a9f959222519ca76099f0a
galu7777 <luisgalea31@gmail.com>
2023-08-27 12:01:24 -0400

    fix<app/models> update model user

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,6 +1,7 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon'
 import Encryption from '@ioc:Adonis/Core/Encryption'
-import { BaseModel, column, HasMany, hasMany } from "@ioc:Adonis/Lucid/Orm";
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
 import Task from './Task';
 
 export default class User extends BaseModel {
@@ -19,12 +20,22 @@ export default class User extends BaseModel {
   })
   public password: string;
 
+  @column()
+  public rememberMeToken: string | null
+
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime;
+  public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime;
+  public updatedAt: DateTime
 
   @hasMany(() => Task)
   public tasks: HasMany<typeof Task>
+
+  @beforeSave()
+  public static async hashPassword (user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 }
