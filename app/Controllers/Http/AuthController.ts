a9913317bfad9e168513bfbcf4279bff9d3e18bd
galu7777 <luisgalea31@gmail.com>
2023-09-11 13:31:26 -0400

    feat<app/controllers/http>: login by email

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -5,15 +5,26 @@ import User from "App/Models/User";
 export default class AuthController {
   
   public async login({ auth, request, response }: HttpContextContract) {
+    // const { nick_name, email, password } = request.body()
     const nickName = request.input("nick_name");
+    const email = request.input("email");
     const password = request.input("password");
 
-    const user = await User.findByOrFail("nick_name", nickName);
-
-    if (!(await Hash.verify(user.password, password))) {
-      return response.unauthorized("Invalid credentials");
+    if(nickName) {
+      const user = await User.findByOrFail("nick_name", nickName);
+      if (!(await Hash.verify(user.password, password))) {
+        return response.unauthorized("Invalid credentials");
+      }
+  
+      return await auth.use("api").generate(user);
+    } else {
+      const user = await User.findByOrFail("email", email);
+      if (!(await Hash.verify(user.password, password))) {
+        return response.unauthorized("Invalid credentials");
+      }
+  
+      return await auth.use("api").generate(user);
     }
-
-    return await auth.use("api").generate(user);
+    
   }
 }
