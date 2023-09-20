e3c8d602717b34bc1eaa46927f92afaeb6187f48
galu7777 <luisgalea31@gmail.com>
2023-09-19 22:56:58 -0400

    feat<app/controllers/http>: add more methods

--- app/Controllers/Http/BaseController.ts
+++ app/Controllers/Http/BaseController.ts
@@ -1,22 +1,68 @@
 import { ResponseContract } from "@ioc:Adonis/Core/Response";
+import { RequestContract } from "@ioc:Adonis/Core/Request";
 import { AuthContract } from "@ioc:Adonis/Addons/Auth";
 import User from "App/Models/User";
+import { createNewTask, destroyTask, updateTask } from "../../../helpers/helpersControllers/HelperTask"
+import { createNewUser, updateUser } from "../../../helpers/helpersControllers/HelperUser"
 
 export class BaseController {
   protected auth: AuthContract | User | any;
+  protected user: {} | any;
   protected response: ResponseContract;
+  protected request: RequestContract;
+  protected params: Record<string, any>;
+  protected task: {} | any ; 
 
-  constructor({ response }: { response: ResponseContract }) {
+  constructor({ response, request }: { response: ResponseContract, request: RequestContract }, ) {
     this.response = response;
+    this.request = request;
   }
-
-  async init({ auth }: { auth: AuthContract }) {
+  
+  protected async init({ auth }: { auth: AuthContract }) {
     try {
       this.auth = await auth.use("api").authenticate();
     } catch (error) {
       console.error(error);
     }
   }
+
+  protected async postUser({request }: { request: RequestContract }){
+    this.user = await createNewUser(request)
+  }
+
+  protected async putUser(
+    { auth, 
+      request,
+      params 
+    }: { 
+      auth: AuthContract, 
+      request: RequestContract,
+      params: Record<string, any>}
+  ){
+    this.task = await updateUser(auth, request, params)
+  }
+
+  protected async postTask({ auth, request }: { auth: AuthContract, request: RequestContract }){
+    this.task = await createNewTask(auth, request)
+  }
+
+  protected async putTask(
+    { auth, 
+      request,
+      params 
+    }: { 
+      auth: AuthContract, 
+      request: RequestContract,
+      params: Record<string, any>}
+    ){
+    this.task = await updateTask(auth, request, params)
+  }
+
+  protected async deleteTask({ auth, params}: {auth: AuthContract, params: Record<string, any>}){
+    this.task = await destroyTask(auth, params)
+  }
+
+
 
   protected sendResponse(
     result: object | string | null = null,
