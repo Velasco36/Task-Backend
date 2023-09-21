69799bcb9d4f443cf11dcb5fb4156f9b90438d08
galu7777 <luisgalea31@gmail.com>
2023-09-21 10:54:52 -0400

    feat<app/controllers/http>: merge fixed/contr-rout

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,93 +1,47 @@
-import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
-import { schema } from "@ioc:Adonis/Core/Validator";
-import User from "App/Models/User";
+// import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import { BaseController } from "./BaseController";
 
-export default class UsersController {
-  public async index({ auth, response }: HttpContextContract) {
-    try {
-      const data = await User.query().preload("tasks");
-      const payload = data.map((info) => ({
-        user: info.$original,
-        tasks: info.$preloaded.tasks
-      }))
-      const user = payload.map((data) => (
-       {
-        id: data.user.id,
-        nickName: data.user.nick_name,
-        email:  data.user.email,
-        task: data.tasks
-       }
-      ))
-      await auth.use("api").authenticate()
-      return response.status(200).json(user)
+export default class UsersController extends BaseController {
+  public async index() {
+    try {      
+      const user = this.auth();
+      return this.sendResponse(user);
     } catch (error) {
-      return response.status(404).json({ message: "An error has occurred" });
+      return this.sendError(error);
     }
   }
 
-  public async store({ response, request }: HttpContextContract) {
+  public async store() {
     try {
-      const newUserSchema = schema.create({
-        nick_name: schema.string({ trim: true }),
-        email: schema.string({ trim: true }),
-        password: schema.string({ trim: true }),
-      });      
-      const payload = await request.validate({ schema: newUserSchema });
-      const user = await User.create(payload);
-      return response.status(200).json(user);      
+      this.postUser
+      return this.sendResponse(this.user);
     } catch (error) {
-      return response.status(404).json({ message: "An error has occurred" });
+      return this.sendError(error);
     }
   }
-  public async show({ auth, params, response }: HttpContextContract) {
+  public async show() {
     try {
-      const user = await User.query()
-        .where('id', params.id)
-        .preload('tasks')
-        // .firstOrFail();
-
-        const responseData = user.map((info)=> ({
-          id: info.id,
-          nick_name:info.nick_name,
-          email: info.email,
-          tasks: info.$preloaded.tasks
-        }))
-      await auth.use("api").authenticate()
-      return response.status(200).json(responseData);
+      return this.sendResponse({ user: this.auth });
     } catch (error) {
-      return response.status(404).json({ message: "An error has occurred" });
+      this.sendError(error);
     }
   }
 
-  public async update({ auth, params, request, response }: HttpContextContract) {
+  public async update() {
     try {
-      const body = request.body();
-      const user = await User.findOrFail(params.id);
-      if (body.nick_name) {
-        user.nick_name = body.nick_name;
-      }
-      if (body.email) {
-        user.email = body.email;
-      }
-      if (body.password) {
-        user.password = body.password;
-      }
-      await auth.use("api").authenticate()
-      await user.save();
-      response.status(200).json({ message: "User Updated Successfully", user });
+      this.putUser
+      return this.sendResponse(this.user);
     } catch (error) {
-      return response.status(404).json({ message: "An error has occurred" });
+      return this.sendError(error);
     }
   }
 
-  public async destroy({ params, response }: HttpContextContract) {
+  public async destroy() {
     try {
-      const user = await User.findOrFail(params.id);
-      await user.delete();
-      response.status(200).json({ message: "User Deleted Successfully", user });
+      this.deleteUser
+      return this.sendResponse(this.user);
     } catch (error) {
-      console.log({ error: error });
-      return response.status(404).json({ message: "An error has occurred" });
+      return this.sendError(error);
     }
   }
 }
